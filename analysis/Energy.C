15af33e852cf94801b7bdc2e41a3d294e0a23a22
lyazj <seeson@pku.edu.cn>
2024-09-02 11:26:32 +0800

    Validate Estd computation

--- analysis/Energy.C
+++ analysis/Energy.C
@@ -1,10 +1,14 @@
 #include "../include/Object.hh"
 #include <TFile.h>
 #include <TTree.h>
+#include <TClonesArray.h>
 #include <TH1.h>
 #include <TCanvas.h>
 #include <TStyle.h>
 #include <TROOT.h>
+#include <iostream>
+#include <iomanip>
+#include <unordered_map>
 
 using namespace std;
 
@@ -24,13 +28,33 @@ void Energy(const char *path)
   auto Energy = (TH1 *)gROOT->FindObject("htemp");
   Energy->SetName("Energy");
   Energy->SetTitle("");
-
   Energy->SetXTitle("Energy [MeV]");
   Energy->SetYTitle("Events");
   Energy->Draw();
 
   canvas->SaveAs("Energy.pdf");
-
   delete canvas;
+
+  unordered_map<Double_t, Long64_t> Ecount;
+  Double_t ESum = 0, E2Sum = 0;
+  TClonesArray *Tracks = NULL;
+  tree->SetBranchAddress("Tracks", &Tracks);
+  for(Long64_t i = 0; tree->GetEntry(i); ++i)
+  {
+    auto track = (Track *)Tracks->At(0);  // incoming beam
+    ++Ecount[track->E];
+    ESum += track->E;
+    E2Sum += track->E * track->E;
+  }
+  cout << scientific << setprecision(18);
+  cout << "Mean energy: " << ESum / tree->GetEntries() << " MeV" << endl;
+  cout << "Standard deviation: "
+       << sqrt(E2Sum / tree->GetEntries() - (ESum / tree->GetEntries()) * (ESum / tree->GetEntries()))
+       << " MeV" << endl;
+  cout << "Energy distribution:" << endl;
+  for(auto &p : Ecount) {
+     cout << "  " << p.first << " MeV\t" << p.second << " events" << endl;
+  }
+
   file->Close();
 }
