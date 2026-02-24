062c7af2dd3504a2a3a86941bab23e05ec32b869
youpeng <wuyoupeng2001@outlook.com>
2026-02-24 13:48:23 +0800

    add more data and update gen_particle script

--- mudirac_data/generate/gen_particle.py
+++ mudirac_data/generate/gen_particle.py
@@ -1,20 +1,22 @@
+#!/usr/bin/env python3
 import os
 import subprocess
+import argparse
 
 # configuration
 
-# ELEMENT = "Th"
-# Z = 90
-# ISOTOPE = 229
-# MAX_N = 14  
+ELEMENT = "Th"
+Z = 90
+ISOTOPE = 232
+MAX_N = 5
 # ELEMENT = "Cu"
 # Z = 29
 # ISOTOPE = 63
 # MAX_N = 14
-ELEMENT = "Al"
-Z = 13
-ISOTOPE = 27
-MAX_N = 14
+# ELEMENT = "Al"
+# Z = 13
+# ISOTOPE = 27
+# MAX_N = 14
 OUTPUT_DIR = "./.."
 MUDIRAC_EXEC = "mudirac" 
 SHELL_MAP = {
@@ -68,44 +70,65 @@ def get_transitions(n_initial, n_final):
     return lines
 
 # main
+
+def run(args):
     
-if not os.path.exists(OUTPUT_DIR):
-    os.makedirs(OUTPUT_DIR)
+    ELEMENT = args.element
+    Z = args.atomic_number
+    ISOTOPE = args.isotope
+    MAX_N = args.max_n
+
+    if not os.path.exists(OUTPUT_DIR):
+        os.makedirs(OUTPUT_DIR)
 
 
-for ni in range(2, MAX_N + 1): 
-    for nf in range(1, ni):      
-        lines = get_transitions(ni, nf)
-        if not lines:
-            print(f"Skipping n={ni} -> n={nf} (No lines defined)")
-            continue
-        lines_str = ",".join(lines)
-        job_name = f"{ELEMENT}{ISOTOPE}_N{ni}_to_N{nf}"
-        in_file = f"{job_name}.in"
-        
-
-        with open(in_file, "w") as f:
-            f.write(f"element: {ELEMENT}\n")
-            f.write(f"isotope: {ISOTOPE}\n")
-            f.write(f"xr_lines: {lines_str}\n")
-            f.write(f"write_spec: T\n") 
-            f.write(f"nuclear_model: FERMI2\n")
-            f.write(f"electronic_config: {ELEMENT}\n")
-            f.write(f"uehling_correction: T\n")
-        
-        print(f"Running {job_name} [Lines: {lines_str}] ...")
-        
-        try:
-            subprocess.run([MUDIRAC_EXEC, in_file], check=True, stdout=subprocess.DEVNULL)
-            generated_spec = f"{job_name}.spec.dat"
+    for ni in range(2, MAX_N + 1): 
+        for nf in range(1, ni):      
+            lines = get_transitions(ni, nf)
+            if not lines:
+                print(f"Skipping n={ni} -> n={nf} (No lines defined)")
+                continue
+            lines_str = ",".join(lines)
+            job_name = f"{ELEMENT}{ISOTOPE}_N{ni}_to_N{nf}"
+            in_file = f"{job_name}.in"
             
-            if os.path.exists(generated_spec):
-                target_name = f"Z{Z}_A{ISOTOPE}_Ni{ni}_Nf{nf}.dat"
-                target_path = os.path.join(OUTPUT_DIR, target_name)
-                os.rename(generated_spec, target_path)
-                print(f" -> Saved to {target_path}")
-            else:
-                print(f" -> Error: Output file {generated_spec} not found.")
 
-        except Exception as e:
-            print(f" -> Failed: {e}")
+            with open(in_file, "w") as f:
+                f.write(f"element: {ELEMENT}\n")
+                f.write(f"isotope: {ISOTOPE}\n")
+                f.write(f"xr_lines: {lines_str}\n")
+                f.write(f"write_spec: T\n") 
+                f.write(f"nuclear_model: FERMI2\n")
+                f.write(f"electronic_config: {ELEMENT}\n")
+                f.write(f"uehling_correction: T\n")
+            
+            print(f"Running {job_name} [Lines: {lines_str}] ...")
+            
+            try:
+                subprocess.run([MUDIRAC_EXEC, in_file], check=True, stdout=subprocess.DEVNULL)
+                generated_spec = f"{job_name}.spec.dat"
+                
+                if os.path.exists(generated_spec):
+                    target_name = f"Z{Z}_A{ISOTOPE}_Ni{ni}_Nf{nf}.dat"
+                    target_path = os.path.join(OUTPUT_DIR, target_name)
+                    os.rename(generated_spec, target_path)
+                    print(f" -> Saved to {target_path}")
+                else:
+                    print(f" -> Error: Output file {generated_spec} not found.")
+
+            except Exception as e:
+                print(f" -> Failed: {e}")
+
+# args
+def main():
+    parser = argparse.ArgumentParser(description="Generate MuDirac input files and run simulations.")
+    parser.add_argument("-e", "--element", type=str,  help="Element symbol (example: Th)", required=True)
+    parser.add_argument("-z", "--atomic_number", type=int, help="Atomic number ", required=True)
+    parser.add_argument("-a", "--isotope", type=int, help="Isotope mass number ", required=True)
+    parser.add_argument("-n", "--max_n", type=int, help="Maximum principal quantum number", required=True)
+    # 
+    args = parser.parse_args()
+    run(args)
+    
+if __name__ == "__main__":
+    main()
