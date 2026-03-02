8e6949e9fd1dd32069cb9d99cb77ae7adaa1be40
lyazj <seeson@pku.edu.cn>
2026-03-02 18:18:54 +0800

    Update run scripts

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PID_BEG="" PID_END="" INPUTS="" TEMPS=""
-for i in $(seq $(nproc)); do
+for i in $(seq 100); do
     ./mudet run.mac &
     [ -z "${PID_BEG}" ] && PID_BEG="$!"
     PID_END="$!"
