c6e427e603b869cfb231abc022d8824ce8cc9b34
lyazj <seeson@pku.edu.cn>
2026-03-02 18:16:40 +0800

    Update bin number

--- analysis/muhpge.py
+++ analysis/muhpge.py
@@ -15,8 +15,8 @@ total_edep = ak.sum(events["Edeps.Edep"], axis=1).to_numpy()
 
 for i, cut in enumerate([(0, 10), (6.05, 6.55), (9.29, 9.39)], 1):
     plt.subplot(1, 3, i)
-    plt.hist(gamma_energies, bins=200, range=cut, histtype="step", label=f"Geant4/MuDirac $E_\\gamma$")
-    plt.hist(total_edep, bins=200, range=cut, histtype="step", label=f"HPGe $E_\\mathrm{{dep}}$")
+    plt.hist(gamma_energies, bins=100, range=cut, histtype="step", label=f"Geant4/MuDirac $E_\\gamma$")
+    plt.hist(total_edep, bins=100, range=cut, histtype="step", label=f"HPGe $E_\\mathrm{{dep}}$")
     plt.xlabel(r"$E$ [MeV]")
     plt.ylabel("Events")
     plt.legend()
