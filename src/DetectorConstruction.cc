e9163758bc949e2f84552d250ccb1764a1c4e286
xuebaxiaohei <huzimo2008@126.com>
2026-03-20 16:02:49 +0800

    Fill the last half of the world with CaCO3

--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -31,6 +31,7 @@
 #include "G4PVPlacement.hh"
 #include "G4SystemOfUnits.hh"
 #include "G4NistManager.hh"
+#include "G4Material.hh"
 
 DetectorConstruction::DetectorConstruction()
 {
@@ -49,8 +50,17 @@ G4VPhysicalVolume *DetectorConstruction::Construct()
 {
   auto nist = G4NistManager::Instance();
   auto world_s = new G4Box("world", fWorldX * 0.5, fWorldY * 0.5, fWorldZ * 0.5);
-  auto world_l = new G4LogicalVolume(world_s, nist->FindOrBuildMaterial("G4_WATER"), "world");
-  auto world_p = new G4PVPlacement(NULL, {}, world_l, "world", NULL, false, 0, true);
+  auto world_l = new G4LogicalVolume(world_s, nist->FindOrBuildMaterial("G4_AIR"), "world");
 
+  auto CaCO3 = new G4Material("CaCO3", 2.71 * g / cm3, 3, G4State::kStateSolid);
+  CaCO3->AddElement(nist->FindOrBuildElement(20), 1);
+  CaCO3->AddElement(nist->FindOrBuildElement(6), 1);
+  CaCO3->AddElement(nist->FindOrBuildElement(8), 3);
+
+  auto ground_s = new G4Box("ground", fWorldX * 0.5, fWorldY * 0.5, fWorldZ * 0.25);
+  auto ground_l = new G4LogicalVolume(ground_s, CaCO3, "ground");
+  new G4PVPlacement(NULL, {0, 0, fWorldZ * 0.25}, ground_l, "ground", world_l, false, 0, true);
+
+  auto world_p = new G4PVPlacement(NULL, {}, world_l, "world", NULL, false, 0, true);
   return world_p;
 }
