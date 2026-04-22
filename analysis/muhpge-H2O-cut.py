093b37110915fdbdc999a6b766098aa7522f37ee
lyazj <seeson@pku.edu.cn>
2026-04-22 21:16:21 +0800

    Update scripts

--- analysis/muhpge-H2O-cut.py
+++ analysis/muhpge-H2O-cut.py
@@ -7,7 +7,7 @@ file = uproot.open(f"../build/tree/latest.root")
 events = file["tree"].arrays(["Edeps.Edep", "Tracks.E", "Tracks.Pid"])
 print(f"Number of events: {len(events)}")
 edeps = ak.sum(events["Edeps.Edep"], axis=1)
-events = events[(edeps >= 0.13) & (edeps <= 0.14)]
+events = events[(edeps >= 0.12) & (edeps <= 0.15)]
 del edeps
 print(f"Number of events passing selection: {len(events)}")
 ak.to_parquet(events, f"../build/tree/latest.parquet")
---
+++ analysis/muhpge-H2O-fit.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import awkward as ak
+import numpy as np
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(15, 4))
+
+nmuon = 351560897
+nmuon_one_year = 19 * 19 * 86400 * 365 // 60
+print(f"Number of incident muons: {nmuon} / {nmuon_one_year}")
+weight = nmuon_one_year / nmuon
+events = ak.from_parquet(f"../build/tree/latest.parquet")
+#events = uproot.open(f"../build/tree/latest.root")["tree"].arrays(["Edeps.Edep", "Tracks.E", "Tracks.Pid", "Events.Pid"])
+print(f"Number of events: {len(events)}")
+events = events[ak.num(events["Edeps.Edep"], axis=1) >= 1]
+print(f"Number of events with at least one energy deposition: {len(events)}")
+#events = events[ak.num(events["Events.Pid"], axis=1) > 1]
+#print(f"Number of events with at least one particle on target: {len(events)}")
+
+gamma_energies = events["Tracks.E"][events["Tracks.Pid"] == 22]
+gamma_energies = ak.flatten(gamma_energies).to_numpy()
+print(f"Number of gammas: {len(gamma_energies)}")
+total_edep = ak.sum(events["Edeps.Edep"], axis=1).to_numpy()
+
+for i, (cut, bins) in enumerate([((0.050, 0.200), 150), ((0.130, 0.140), 10), ((0.155, 0.165), 10)], 1):
+    plt.subplot(1, 3, i)
+
+    n, bins_, _ = plt.hist(gamma_energies, bins=bins, range=cut, weights=np.ones_like(gamma_energies) * weight, histtype="step", color="blue", label=f"Geant4/MuDirac $E_\\gamma$")
+    plt.errorbar(0.5 * (bins_[:-1] + bins_[1:]), n, yerr=np.maximum(np.sqrt(n), np.sqrt(n / weight) * weight), fmt="none", color="blue", elinewidth=1.0, capsize=0)
+
+    n, bins_, _ = plt.hist(total_edep, bins=bins, range=cut, weights=np.ones_like(total_edep) * weight, histtype="step", color="red", label=f"HPGe $E_\\mathrm{{dep}}$")
+    plt.errorbar(0.5 * (bins_[:-1] + bins_[1:]), n, yerr=np.maximum(np.sqrt(n), np.sqrt(n / weight) * weight), fmt="none", color="red", elinewidth=1.0, capsize=0)
+
+    plt.xlabel(r"$E$ [MeV]")
+    plt.ylabel("Events / year")
+    plt.legend()
+    plt.grid()
+
+plt.tight_layout()
+plt.savefig(f"muhpge_latest.pdf")
