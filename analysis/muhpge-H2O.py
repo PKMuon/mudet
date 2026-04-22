1d07d1f01cef97ede93e645e42f0c246bc291d9b
lyazj <seeson@pku.edu.cn>
2026-04-22 19:35:18 +0800

    Update muhpge-H2O.py

--- analysis/muhpge-H2O.py
+++ analysis/muhpge-H2O.py
@@ -29,10 +29,10 @@ for i, (cut, bins) in enumerate([((0.050, 0.200), 150), ((0.130, 0.140), 10), ((
     plt.subplot(1, 3, i)
 
     n, bins_, _ = plt.hist(gamma_energies, bins=bins, range=cut, weights=np.ones_like(gamma_energies) * weight, histtype="step", color="blue", label=f"Geant4/MuDirac $E_\\gamma$")
-    plt.errorbar(0.5 * (bins_[:-1] + bins_[1:]), n, yerr=np.sqrt(n / weight) * weight, fmt="none", color="blue", elinewidth=1.0, capsize=0)
+    plt.errorbar(0.5 * (bins_[:-1] + bins_[1:]), n, yerr=np.maximum(np.sqrt(n), np.sqrt(n / weight) * weight), fmt="none", color="blue", elinewidth=1.0, capsize=0)
 
     n, bins_, _ = plt.hist(total_edep, bins=bins, range=cut, weights=np.ones_like(total_edep) * weight, histtype="step", color="red", label=f"HPGe $E_\\mathrm{{dep}}$")
-    plt.errorbar(0.5 * (bins_[:-1] + bins_[1:]), n, yerr=np.sqrt(n / weight) * weight, fmt="none", color="red", elinewidth=1.0, capsize=0)
+    plt.errorbar(0.5 * (bins_[:-1] + bins_[1:]), n, yerr=np.maximum(np.sqrt(n), np.sqrt(n / weight) * weight), fmt="none", color="red", elinewidth=1.0, capsize=0)
 
     plt.xlabel(r"$E$ [MeV]")
     plt.ylabel("Events / year")
