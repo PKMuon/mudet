9411563d9c8b94d88ed6be876c7f99fd5756c6a2
lyazj <seeson@pku.edu.cn>
2026-04-22 21:10:02 +0800

    Fix muhpge-H2O-cut.py

--- analysis/muhpge-H2O-cut.py
+++ analysis/muhpge-H2O-cut.py
@@ -6,8 +6,8 @@ import awkward as ak
 file = uproot.open(f"../build/tree/latest.root")
 events = file["tree"].arrays(["Edeps.Edep", "Tracks.E", "Tracks.Pid"])
 print(f"Number of events: {len(events)}")
-events = events[ak.num(events["Edeps.Edep"], axis=1) >= 1]
-print(f"Number of events with at least one energy deposition: {len(events)}")
-events = events[events["Edeps.Edep"] >= 0.05]
-print(f"Number of events with energy deposition >= 0.05 MeV: {len(events)}")
-ak.to_parquet(f"../build/tree/latest.parquet", events)
+edeps = ak.sum(events["Edeps.Edep"], axis=1)
+events = events[(edeps >= 0.13) & (edeps <= 0.14)]
+del edeps
+print(f"Number of events passing selection: {len(events)}")
+ak.to_parquet(events, f"../build/tree/latest.parquet")
